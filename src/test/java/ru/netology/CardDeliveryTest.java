6d1369f7e11db52f1fc7aee62e02a807df0e24f8
Gubina Irina <irina.gubina98@mail.ru>
2020-12-22 19:35:56 +0300

    Добавлены тесты по взаимодействию с копмлексными элементами

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,8 +1,10 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.KeyDownAction;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -15,20 +17,60 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
-    @Test
-    void shouldArrangeDelivery() throws InterruptedException {
+    @BeforeEach
+    void openWebPage() {
         open("http://localhost:9999");
+    }
 
+    @Test
+    void shouldArrangeDelivery() {
         $("[data-test-id='city'] input").setValue("Москва");
         LocalDate date = LocalDate.now();
         String dateTest = date.plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("input[placeholder=\"Дата встречи\"]").setValue(dateTest);
         $("[data-test-id='name'] input").setValue("Губина Ирина");
         $("[data-test-id='phone'] input").setValue("+79650001122");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
 
-        $("[data-test-id='notification']").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateTest));
+        $("[data-test-id='notification']").waitUntil(visible, 15000)
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateTest));
+    }
+
+    @Test
+    void selectingACityFromTheList() {
+        $("[data-test-id='city'] input").setValue("Мо")
+                .sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER));
+        LocalDate date = LocalDate.now();
+        String dateTest = date.plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("input[placeholder=\"Дата встречи\"]").setValue(dateTest);
+        $("[data-test-id='name'] input").setValue("Губина Ирина");
+        $("[data-test-id='phone'] input").setValue("+79650001122");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+
+        $("[data-test-id='notification']").waitUntil(visible, 15000)
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateTest));
+    }
+
+    @Test
+    void dateSelection() {
+        $("[data-test-id='city'] input").setValue("Москва");
+        LocalDate date = LocalDate.now();
+        String dateTest = date.plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT,
+                        Keys.ARROW_RIGHT, Keys.ENTER));
+        $("[data-test-id='name'] input").setValue("Губина Ирина");
+        $("[data-test-id='phone'] input").setValue("+79650001122");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+
+        $("[data-test-id='notification']").waitUntil(visible, 15000)
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateTest));
     }
 }
